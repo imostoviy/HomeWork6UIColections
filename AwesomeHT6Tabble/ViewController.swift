5bc479e49a106d0b875d53b9c5453d31e22b50d6
imostoviy <i_mostoviy@ukr.net>
2018-12-06 07:18:26 +0200

    Initial Commit

---
+++ AwesomeHT6Tabble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeHT6Tabble
+//
+//  Created by Мостовий Ігор on 06.12.18.
+//  Copyright © 2018 Мостовий Ігор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
